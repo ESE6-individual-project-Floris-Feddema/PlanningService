80b659294d935e81094ef4b1774eb30c56da3620
Floris Feddema <floris1996@hotmail.com>
2020-06-16 01:54:06 +0200

    PLAN-92: create function setup

--- PlanningService/PlanningService/SecondHttpTrigger.cs
+++ PlanningService/PlanningService/SecondHttpTrigger.cs
@@ -10,11 +10,11 @@ using Newtonsoft.Json;
 
 namespace PlanningService
 {
-    public static class HttpExample
+    public static class SecondHttpTrigger
     {
-        [FunctionName("HttpExample")]
+        [FunctionName("SecondHttpTrigger")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
