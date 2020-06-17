1c2be573f3f36368da43fb8a4097c2419d02fc94
Floris Feddema <floris1996@hotmail.com>
2020-06-17 13:20:39 +0200

    PLAN-82: adds empty create planning document

---
+++ PlanningService/Planning.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanningService
+{
+    public class Planning
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+    }
+}
